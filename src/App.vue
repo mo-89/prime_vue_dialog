1d6a48b562b509b3a780f91df9cc0e15a405f0f5
mo <mo@monoMacBook-Air.local>
2023-08-05 23:52:20 +0900

    divide modal

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,42 @@
 <template>
-  <Button label="open modal" @click="showDialog" />
-
-  <Dialog :visible="display" :modal="true" :style="{ width: '50vw'}" :closable="false" @hide="onHide">  
-    <template #header>
-      <h2>Input form</h2>
-    </template>
-    <div class="p-fluid">
-      <div class="p-filed">
-        <label for="input1">input 1</label>
-        <InputText id="input1" v-model="input1" />
-      </div>
-      <div class="p-filed">
-        <label for="input2">input 2</label>
-        <InputText id="input2" v-model="input2" />
-      </div>
-    </div>
-    <template #footer>
-      <Button label="Cancel" class="p-button-text" @click="onHide" />
-      <Button label="Submit" class="p-button-success" @click="submit" />
-    </template>
-  </Dialog>
+  <div>
+    <Button label="create new" @click="display = true" />
+    <MyModal 
+      :display="display"
+      @update:display="display = $event"
+      :createRecord="createRecord"
+    ></MyModal>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
-import Dialog from 'primevue/dialog'
 import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
+import MyModal from './components/MyModal.vue'
 
 export default {
   components: {
-    Dialog,
     Button,
-    InputText
+    MyModal
   },
   data() {
     return {
       display: false,
-      input1: '',
-      input2: '',
-    }
+      records: []
+    };
+  },
+  async created() {
+    // const response = await axios.get('/api/records')
+      const response = []
+    this.records = response.data
   },
   methods: {
-    showDialog() {
-      this.display = true;
-    },
-    onHide() {
-      this.display = false;
-    },
-
-    async submit() {
-      try {
-        const response = await axios.post('api/test', {
-          input1: this.input1,
-          input2: this.input2,
-        })
-        console.log(response.data)
-
-        this.onHide
-      } catch (error) {
-        console.log(error)
-      }
+    async createRecord(record) {
+      await axios.post('api/create', record)
+      // const response = await axios.get('/api/records')
+      const response = []
+      this.records = response.data
+      this.display = false
     }
   }
 }
---
+++ src/components/MyModal.vue
@@ -0,0 +1,63 @@
+
+<template>
+  <Dialog :visible="localDisplay" :modal="true" :closable="false">
+    <template #header>
+      <h2>Input form</h2>
+    </template>
+    <div class="p-fluid">
+      <div class="p-filed">
+        <label for="input1">input 1</label>
+        <InputText id="input1" v-model="record.input1" />
+      </div>
+      <div class="p-filed">
+        <label for="input2">input 2</label>
+        <InputText id="input2" v-model="record.input2" />
+      </div>
+    </div>
+    <template #footer>
+      <Button label="create" @click="create" />
+      <Button label="close" @click="close" />
+    </template>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from 'primevue/dialog'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+export default {
+  components: {
+    Dialog,
+    Button,
+    InputText
+  },
+  props: {
+    display: Boolean,
+    createRecord: Function
+  },
+  data() {
+    return {
+      localDisplay: this.display,
+      record: {
+        input1: '',
+        input2: ''
+      }
+    };
+  },
+  watch: {
+    display(newVal) {
+      this.localDisplay = newVal
+    }
+  },
+  methods: {
+    create() {
+      this.createRecord(this.record)
+    },
+    close() {
+      this.localDisplay = false
+      this.$emit('update:display', false)
+    }
+  }
+};
+</script>
